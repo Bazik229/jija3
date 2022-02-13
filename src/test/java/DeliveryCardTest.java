7fc9340dafa2f7109ff8878d0d0be1de80398715
Bazik229 <azik229g@gmail.com>
2022-02-13 22:43:12 +0400

    init

---
+++ src/test/java/DeliveryCardTest.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Configuration;
+
+import com.codeborne.selenide.ElementsCollection;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class DeliveryCardTest {
+
+    @Test
+    void shouldDeliveryCard(){
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+
+        ElementsCollection inputs = $$(byClassName("input__control"));
+        inputs.get(0).val("Самара");
+        inputs.get(1).val("13.02.2022");
+        inputs.get(2).val("Иван Петров");
+        inputs.get(3).val("+79277163582");
+
+        $(byClassName("checkbox__box")).click();
+        $(byClassName("button__content")).click();
+
+        $(byClassName("notification__title")).shouldBe(visible).shouldHave(text("Успешно!"), Duration.ofSeconds(14));
+    }
+}
